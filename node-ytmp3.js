075073b79ab1e5c29af81393f9bfec1d24f3f530
drowzyorginal <simon.thornqvist@gmail.com>
2013-08-26 16:13:44 +0200

    glorious 2 spaces indent, option to convert

--- node-ytmp3.js
+++ node-ytmp3.js
@@ -10,44 +10,37 @@ program
   .version('0.0.1')
   .option('-u, --url <url>', 'youtube url')
   .option('-o, --output <string>', 'output file')
+  .option('-c, --convert', 'File will be converted to mp3')
   .parse(process.argv);
 
-
     if(program.url && program.output) {
-        console.log("Downloading");
-        dlyt(program.url, program.output , true);
+      console.log("Downloading");
+      dlyt(program.url, program.output , program.covert);
     }
     else {
-        console.log("URL or output file not defined");
+      console.log("URL or output file not defined");
     }
 
 function dlyt(path, destination, convertToMP3){
-    var myytld = ytdl(path);
-    var vidstream = fs.createWriteStream(destination);
-    myytld.pipe(vidstream);
-
-    vidstream.on('close', function (){
-        console.log("Download complete");
-        if(convertToMP3) {
-            var fileEnd = /\.flv$/;
-            convert(destination, destination.replace(fileEnd, '.mp3'));
-        }
-    });
-
+  var myytld = ytdl(path);
+  var vidstream = fs.createWriteStream(destination);
+  myytld.pipe(vidstream);
+  vidstream.on('close', function (){
+    console.log("Download complete");
+    if(convertToMP3) {
+      console.log("converting to mp3");
+      var fileEnd = /\.flv$/;
+      convert(destination, destination.replace(fileEnd, '.mp3'));
+    }
+  });
 }
 
 function convert (source, destination) {
-
-    var proc = new ffmpeg({ source: source , nolog: false})
-        .withAudioCodec('libmp3lame')
-        .toFormat('mp3')
-        .saveToFile(destination, function( stdout, stderr){
-            if(stderr){
-                console.log(stderr);
-            }
-            else{
-                console.log('file converted succesfully');
-            }
+  var proc = new ffmpeg({ source: source , nolog: false})
+    .withAudioCodec('libmp3lame')
+    .toFormat('mp3')
+    .saveToFile(destination, function( stdout, stderr){
+      stderr ? console.log(stderr) : console.log('file converted successfully');
     });
 
 }
