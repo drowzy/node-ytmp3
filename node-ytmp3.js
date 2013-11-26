e11e5b74731715d0f007d7cf6cef4afb52cf5749
drowzyoriginal <simon.thornqvist@gmail.com>
2013-11-26 03:58:45 +0100

    filename are retrived automatically from meta data, removes .flv file if converting to mp3

--- node-ytmp3.js
+++ node-ytmp3.js
@@ -9,40 +9,51 @@ var program = require('commander');
 program
   .version('0.0.1')
   .option('-u, --url <url>', 'youtube url')
-  .option('-o, --output <file>', 'name of the output file')
   .option('-f, --format <format>', 'output format, i.e flv or mp3')
   .parse(process.argv);
 
     if(program.url && program.format) {
-      console.log("Downloading");
+      console.log("----- Downloading -----");
       convertToMp3 = program.format == "mp3" ? true : false;
-      dlyt(program.url, program.output, convertToMp3);
+      dlyt(program.url, convertToMp3);
     }
     else {
-      console.log("URL or output file not defined");
+      console.log("URL or file format not defined");
     }
 
-function dlyt(path, destination, convertToMP3){
-  ytdl(path).pipe(fs.createWriteStream(destination)).on('close', function() {
-    console.log("Download complete");
-    if(convertToMP3) {
-      console.log("converting to mp3");
-      var fileEnd = /\.flv$/;
-      convert(destination, destination.replace(fileEnd, '.mp3'));
-    }
+function dlyt(path, convertToMp3){
+  ytdl.getInfo(path, function(err, info) {
+    var fileName = err ? "ytmp3.flv" : sanatize(info.title.trim() + ".flv");
+    var stream = fs.createWriteStream(fileName);
+    ytdl(path).pipe(stream).on('close', function() {
+      console.log("----- Download complete -----");
+      if(convertToMp3) {
+        stream.end();
+        console.log("----- converting to mp3 -----");
+        var fileEnd = /\.flv$/;
+        convert(fileName, fileName.replace(fileEnd, '.mp3'));
+      }
+    });
   });
+}
+/* Title which starts with a dash will cause ffmpeg to think its an option, 
+so they need to be removed.*/
+function sanatize(title) {
+  
+  while(title.charAt(0) == '-')
+    title = title.substr(1);
+  return title;
 }
 
 function convert (source, destination) {
-  var proc = new ffmpeg({ source: source , nolog: false})
+  var proc = new ffmpeg({ source: source , nolog: true, timeout: 60 })
     .withAudioCodec('libmp3lame')
     .toFormat('mp3')
-    .saveToFile(destination, function(stdout, stderr){
-      if(stderr) {
-        console.log(stderr);
-      } else {
-        console.log('file converted successfully');
-      }
+    .saveToFile(destination, function(stdout, stderr) {
+        console.log('----- file converted successfully! -----');
+        fs.unlink(source, function(err) {
+          console.log("----- Removing temporary file: " + source);         
+        });
     });
 }
 
