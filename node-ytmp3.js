beed2e830349e26ac6b721e7a7909daaea5a5076
drowzyoriginal <simon.thornqvist@gmail.com>
2013-11-26 02:59:59 +0100

    ticked up the dependencies, resolved errors

--- node-ytmp3.js
+++ node-ytmp3.js
@@ -9,23 +9,21 @@ var program = require('commander');
 program
   .version('0.0.1')
   .option('-u, --url <url>', 'youtube url')
-  .option('-o, --output <string>', 'output file')
-  .option('-c, --convert', 'File will be converted to mp3')
+  .option('-o, --output <file>', 'name of the output file')
+  .option('-f, --format <format>', 'output format, i.e flv or mp3')
   .parse(process.argv);
 
-    if(program.url && program.output) {
+    if(program.url && program.format) {
       console.log("Downloading");
-      ytdl(program.url, program.output , program.covert);
+      convertToMp3 = program.format == "mp3" ? true : false;
+      dlyt(program.url, program.output, convertToMp3);
     }
     else {
       console.log("URL or output file not defined");
     }
 
 function dlyt(path, destination, convertToMP3){
-  var myytld = ytdl(path);
-  var vidstream = fs.createWriteStream(destination);
-  myytld.pipe(vidstream);
-  vidstream.on('close', function (){
+  ytdl(path).pipe(fs.createWriteStream(destination)).on('close', function() {
     console.log("Download complete");
     if(convertToMP3) {
       console.log("converting to mp3");
@@ -39,8 +37,12 @@ function convert (source, destination) {
   var proc = new ffmpeg({ source: source , nolog: false})
     .withAudioCodec('libmp3lame')
     .toFormat('mp3')
-    .saveToFile(destination, function( stdout, stderr){
-      stderr ? console.log(stderr) : console.log('file converted successfully');
+    .saveToFile(destination, function(stdout, stderr){
+      if(stderr) {
+        console.log(stderr);
+      } else {
+        console.log('file converted successfully');
+      }
     });
 }
 
